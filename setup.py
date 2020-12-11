376cb6ed99446316b085a9498e2a608ecfd15cc7
Thibaut Castaings <castaings.thibaut@bcg.com>
2020-12-11 14:07:41 +0100

    Uploaded to pypi

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     include_package_data=True,
     url='https://github.com/supercuiller/krakenforwarder',
     license='MIT',
-    author='Thibaut Castaings',
+    author='Thibaut Castaings, Zao Wu',
     author_email='castaings.t@protonmail.com',
     description='Forwards trades from Kraken.com to TCP port',
     long_description=open('README.md').read(),
